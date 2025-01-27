cc52dea979889a9f0f48a2630390591b7ae4875e
Mbiphes <castlegima@gmail.com>
2025-01-27 17:11:55 +1000

    add files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# singularityfinance
+# singularityfinance-auto
+On chain soft in order to automatically do singularity finance testnet 
---
+++ configs/settings.py
@@ -0,0 +1,18 @@
+ENABLED_OPERATIONS = {
+    "wrap": True,
+    "unwrap": True,
+    "claim": True,
+    "stake": True,
+    "unstake": True,
+    "bridge_sfi": True,
+    "bridge_sep": False,
+}
+
+OPERATION_LIMITS: dict[str, tuple[float, float]] = {
+    "wrap": (0.01, 0.2),
+    "unwrap": (0.01, 0.2),
+    "stake": (0.001, 0.01),
+    "unstake": (0.001, 0.05),
+    "bridge_sfi": (0.001, 0.05),
+    "bridge_sep": (0.001, 0.01),
+}
